9bf7a6fb42cb295ff07e39936782613132490c06
Henry Nawrocki <hdev@sztanski.com>
2021-10-25 12:53:24 -0500

    broke homepage into components and adds styling to menu-item

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,20 @@
+import './menu-item.styles.scss'
+
+const MenuItem = ({ title, subtitle, imageUrl, size }) => {
+  return (
+    <div className={size ? `menu-item ${size}` : 'menu-item'}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">{subtitle}</span>
+      </div>
+    </div>
+  )
+}
+
+export default MenuItem
