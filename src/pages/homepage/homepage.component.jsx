66057289e4b843c50e2cf194c03cd72441a305a0
Henry Nawrocki <hdev@sztanski.com>
2021-11-24 09:47:24 -0500

    feature new header component and links to home, shop and contact page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,6 @@ import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
 const HomePage = (props) => {
-  console.log(props)
   return (
     <div className="homepage">
       <Directory />
