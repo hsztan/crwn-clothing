db8745da4e3ea68f6aba0d66456322193beaee7e
Henry Nawrocki <hdev@sztanski.com>
2021-12-07 16:37:34 -0500

    feature add signin-signup page and component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   )
