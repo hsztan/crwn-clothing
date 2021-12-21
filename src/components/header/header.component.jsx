b9c4c82eefcc5230e1e69bad8d14e4207ae64187
Henry Nawrocki <hdev@sztanski.com>
2021-12-21 11:45:48 -0500

    integrated sign out functionality with firebase google auth and styled sign in page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { getAuth } from 'firebase/auth'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.styles.scss'
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link to="/">
@@ -18,6 +19,15 @@ const Header = () => {
         <Link className="option" to="/shop">
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => getAuth().signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,12 +50,13 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
 
-          <CustomButton type="submit"> Sign in </CustomButton>
-
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     )
