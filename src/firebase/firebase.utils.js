4bd9f16d69eb08af798bc2a1d0c0f4fc7a723d01
Henry Nawrocki <hdev@sztanski.com>
2022-01-01 17:49:04 -0500

    implemented ability to store authenticated users into firebase database with Email and Password auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,14 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  getDoc,
-  setDoc,
-} from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
