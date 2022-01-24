ca3e49b71bd6ce9f09baced41b789a32a0df15ed
Henry Nawrocki <hdev@sztanski.com>
2022-01-24 16:54:11 -0500

    fixed setCurrentUser when user is not present

--- src/App.js
+++ src/App.js
@@ -10,27 +10,25 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component'
-import {setCurrentUser} from './redux/user/user.actions'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  
-
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser} = this.props
+    const { setCurrentUser } = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
         onSnapshot(userRef, (snapShot) => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data(),
-            })
+            id: snapShot.id,
+            ...snapShot.data(),
+          })
         })
       } else {
-        console.log(userAuth)
+        setCurrentUser(userAuth)
       }
     })
   }
@@ -53,8 +51,8 @@ class App extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 })
 
 export default connect(null, mapDispatchToProps)(App)
